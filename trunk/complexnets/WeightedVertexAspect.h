1f04afccf65b370df394a1a163a48833b29a723b
elsilmaril@gmail.com <elsilmaril@gmail.com@d6b2b88c-4770-d703-3f31-36b83e89a56a>
2012-02-06 21:33:20 +0000

    graph aspects and weightssvn update

--- trunk/complexnets/WeightedVertexAspect.h
+++ trunk/complexnets/WeightedVertexAspect.h
@@ -2,46 +2,56 @@
 #define WEIGHTED_VERTEX_ASPECT_H
 
 #include <map>
+#include "AdjacencyListVertex.h"
 
 namespace graphpp
 {
-template <class T>
-class WeightedVertexAspect : public T
-{
-public:
-
-    typedef double Weight;
-
-    WeightedGraphAspect(VertexId id) : T(id) {}
-
-    void addEdge(WeightedVertexAspect<T>* other, Weight weight)
+    template <class T>
+    class WeightedVertexAspect : public T
     {
-        T::addEdge(other);
-    }
+    protected:
+        //TODO: make these typedefs private
+        typedef AdjacencyListVertex::VertexId VertexId;
+        typedef double Weight;
+        typedef std::map<VertexId, Weight> NeighborsWeights;
+        typedef AutonomousIterator<NeighborsWeights> WeightsIterator;
+        
+    public:
+        
+        WeightedVertexAspect(VertexId id) : T(id) { }
+        
+        void addEdge(WeightedVertexAspect<T>* other, Weight weight)
+        {
+            T::addEdge(other);
+            weights.insert(std::pair<VertexId, Weight>(other->getVertexId(), weight));
+        }
+        
+        Weight edgeWeight(WeightedVertexAspect<T>* neighbour) const
+        {
+            return weights[neighbour->getVertexId()];
+        }
+        
+        WeightsIterator weightsIterator()
+        {
+            WeightsIterator iter(weights);
+            return iter;
+        }
+        
+        Weight strength() const
+        {
+            WeightsIterator iter(weights);
+            Weight str = 0.0;
+            
+            while(!iter.end())
+                str += iter->second;
 
-    Weight edgeWeight(WeightedVertexAspect<T>* neighbour) const
-    {
-        return weightsp[neighbour->getVertexId()];
-    }
-
-    Weight strength() const
-    {
-        WeightsIterator iter(weights);
-        Weight str = 0.0;
-
-        while (!iter.end())
-            str += iter->second;
-
-        return str;
-    }
-
-private:
-
-    typedef std::map<VertexId, Weight> NeighborsWeights;
-    typedef AutonomousIterator<NeighborsWeights> WeightsIterator;
-
-    NeighborsWeights weights;
-};
+            return str;
+        }
+        
+    private:
+        
+        NeighborsWeights weights;
+    };
 }
 
 #endif
--- trunk/test/WeightedGraphTest.cpp
+++ trunk/test/WeightedGraphTest.cpp
@@ -12,64 +12,69 @@
 
 namespace weightedAspectsTest
 {
-
-using namespace graphpp;
-using namespace std;
-using ::testing::Test;
-
-class WeightedGraphTest : public Test
-{
-
-protected:
-
-    WeightedGraphTest() { }
-
-    virtual ~WeightedGraphTest() { }
-
-
-    virtual void SetUp()
+    
+    using namespace graphpp;
+    using namespace std;
+    using ::testing::Test;
+    
+    class WeightedGraphTest : public Test
     {
-
-    }
-
-    virtual void TearDown()
+        
+        protected:
+            
+            WeightedGraphTest() { }
+            
+            virtual ~WeightedGraphTest () { }
+            
+            
+            virtual void SetUp()
+            {
+                
+            }
+            
+            virtual void TearDown()
+            {
+                
+            }
+        public:
+            typedef WeightedVertexAspect<AdjacencyListVertex> Vertex;
+            typedef AdjacencyListGraph<Vertex> IndexedGraph;
+            typedef AdjacencyListGraph<Vertex, list<Vertex*> > ListGraph;
+            typedef AdjacencyListGraph<Vertex, vector<Vertex*> > VectorGraph;
+            typedef WeightedGraphAspect<Vertex, VectorGraph> WeightedGraph;
+            
+    };
+    
+    
+    TEST_F(WeightedGraphTest, AddWeightedEdgeTest)
     {
+        WeightedGraph ig;
+        //Create vertex
+        Vertex* x = new Vertex(1);
+        
+        //create neighbor vertices
+        Vertex* v1 = new Vertex(2);
+        Vertex* v2 = new Vertex(3);
+        Vertex* v3 = new Vertex(4);
+        Vertex* v4 = new Vertex(5);
+        
+        ig.addVertex(x);
+        ig.addVertex(v1);
+        ig.addVertex(v2);
+        ig.addVertex(v3);
+        ig.addVertex(v4);
+        
+        ig.addEdge(x, v1, 1);
+        ig.addEdge(x, v2, 2);
+        ig.addEdge(x, v3, 3);
+        ig.addEdge(v1, v2, 4);
+        ig.addEdge(x, v4, 5);
 
+        delete x;
+        delete v1;
+        delete v2;
+        delete v3;
     }
-public:
-    typedef WeightedVertexAspect<AdjacencyListVertex> Vertex;
-    typedef AdjacencyListGraph<Vertex> IndexedGraph;
-    typedef AdjacencyListGraph<Vertex, list<Vertex*> > ListGraph;
-    typedef AdjacencyListGraph<Vertex, vector<Vertex*> > VectorGraph;
-    typedef WeightedGraphAspect<VectorGraph> WeightedGraph;
-
-};
-
-
-TEST_F(WeightedGraphTest, AddWeightedEdgeTest)
-{
-    WeightedGraph g;
-    //Create vertex
-    Vertex* x = new Vertex(1);
-
-    //create neighbor vertices
-    Vertex* v1 = new Vertex(2);
-    Vertex* v2 = new Vertex(3);
-    Vertex* v3 = new Vertex(4);
-    Vertex* v4 = new Vertex(5);
-
-    ig.addVertex(x);
-    ig.addVertex(v1);
-    ig.addVertex(v2);
-    ig.addVertex(v3);
-    ig.addVertex(v4);
-
-    ig.addEdge(x, v1, 1);
-    ig.addEdge(x, v2, 2);
-    ig.addEdge(x, v3, 3);
-    ig.addEdge(v1, v2, 4);
-    ig.addEdge(x, v4, 5);
-}
-
+    
 }
 
