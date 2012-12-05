61571f144c5c91e20e83d63b9028b3f23b4863be
paulballesty@gmail.com <paulballesty@gmail.com@d6b2b88c-4770-d703-3f31-36b83e89a56a>
2012-12-05 19:36:34 +0000

    Starting to read instructions from command line

--- src/ComplexNetsGui/src/main.cpp
+++ src/ComplexNetsGui/src/main.cpp
@@ -1,11 +1,24 @@
 #include <QtGui/QApplication>
 
 #include "ComplexNetsGui/inc/mainwindow.h"
+#include "ComplexNetsCmd/cmdline.h"
 
 int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    ComplexNetsGui::MainWindow w;
-    w.show();
-    return a.exec();
+	if (argc == 1) {
+	    QApplication a(argc, argv);
+	    ComplexNetsGui::MainWindow w;
+	    w.show();
+	    return a.exec();
+	} else {
+		struct gengetopt_args_info *args_info = (struct gengetopt_args_info *) malloc(sizeof(struct gengetopt_args_info));
+		
+		if(cmdline_parser(argc, argv, args_info) != 0) {
+			return EXIT_FAILURE;
+		}
+
+		if(args_info->load_network_given) {
+			printf("Load network");
+		}
+	}
 }
