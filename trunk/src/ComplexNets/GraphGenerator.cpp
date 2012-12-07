d68394eeb981d6b23a56c25caa3db8ce2754bc58
ezequiel.scaruli@gmail.com <ezequiel.scaruli@gmail.com@d6b2b88c-4770-d703-3f31-36b83e89a56a>
2012-12-07 15:16:27 +0000

    Moving file-input graph loading to GraphGenerator.

--- trunk/src/ComplexNets/GraphGenerator.cpp
+++ trunk/src/ComplexNets/GraphGenerator.cpp
@@ -1,4 +1,9 @@
 #include "GraphGenerator.h"
+#include "../ComplexNets/IGraphReader.h"
+#include "../ComplexNets/GraphFactory.h"
+#include "../ComplexNets/WeightedGraphFactory.h"
+
+using namespace graphpp;
 
 GraphGenerator *GraphGenerator::instance = NULL;
 
@@ -11,6 +16,35 @@ GraphGenerator *GraphGenerator::getInstance() {
 
 	return instance;
 }
+
+
+Graph GraphGenerator::generateGraphFromFile(string path, bool directed, bool multigraph) {
+	Graph graph = Graph(directed, multigraph);
+
+   	IGraphFactory<Graph, Vertex> *factory = new GraphFactory<Graph, Vertex>();
+    IGraphReader<Graph, Vertex> *reader = factory->createGraphReader();
+	reader->read(graph, path);
+
+	delete reader;
+	delete factory;
+
+	return graph;
+}
+
+
+WeightedGraph GraphGenerator::generateWeightedGraphFromFile(string path, bool directed, bool multigraph) {
+	WeightedGraph graph = WeightedGraph(directed, multigraph);
+
+   	IGraphFactory<WeightedGraph, WeightedVertex> *factory = new WeightedGraphFactory<WeightedGraph, WeightedVertex>();
+    IGraphReader<WeightedGraph, WeightedVertex> *reader = factory->createGraphReader();
+    reader->read(graph, path);
+
+	delete reader;
+	delete factory;
+
+	return graph;
+}
+
 
 Graph GraphGenerator::generateErdosRenyiGraph(unsigned int n, unsigned int p) {
 	Graph graph = new Graph(false, false);
