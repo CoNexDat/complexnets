2e54373a4ce68ebe6f4b3b1327d8e323da97fe03
lulirez@gmail.com <lulirez@gmail.com@d6b2b88c-4770-d703-3f31-36b83e89a56a>
2014-12-17 20:33:27 +0000

    tests

---
+++ trunk/test/DirectedDegreeDistributionTest.cpp
@@ -0,0 +1,307 @@
+/**
+ * 2do cuatrimestre 2014
+ * @author  Luciana Reznik
+ */
+#include <gtest/gtest.h>
+#include <set>
+#include <vector>
+#include <list>
+
+
+#include "../src/ComplexNets/DirectedVertexAspect.h"
+#include "../src/ComplexNets/AdjacencyListVertex.h"
+#include "../src/ComplexNets/AdjacencyListGraph.h"
+#include "../src/ComplexNets/GraphExceptions.h"
+#include "../src/ComplexNets/typedefs.h"
+#include "../src/ComplexNets/IGraphFactory.h"
+#include "../src/ComplexNets/DirectedDegreeDistribution.h"
+#include "../src/ComplexNets/GraphGenerator.h"
+#include "../src/ComplexNets/DirectedGraphFactory.h"
+
+
+namespace directedDegreeDistributionTest
+{
+
+using namespace graphpp;
+using namespace std;
+using ::testing::Test;
+
+class DirectedDegreeDistributionTest : public Test
+{
+
+protected:
+
+    DirectedDegreeDistributionTest() { }
+
+    virtual ~DirectedDegreeDistributionTest() { }
+
+
+    virtual void SetUp()
+    {
+
+    }
+
+    virtual void TearDown()
+    {
+
+    }
+public:
+    typedef DirectedVertexAspect<Vertex> DirectedVertex;
+    
+    typedef AdjacencyListGraph<DirectedVertex> IndexedGraph;
+    typedef AdjacencyListGraph<DirectedVertex, list<DirectedVertex*> > ListGraph;
+    typedef AdjacencyListGraph<DirectedVertex, vector<DirectedVertex*> > VectorGraph;
+
+
+
+};
+
+
+TEST_F(DirectedDegreeDistributionTest, DirectedDegreeDistributionIndexed)
+{
+
+   
+    IndexedGraph ig(true,false);
+    //Create vertex
+    DirectedVertex* x = new DirectedVertex(1);
+
+    //create neighbor vertices
+    DirectedVertex* v1 = new DirectedVertex(2);
+    DirectedVertex* v2 = new DirectedVertex(3);
+    DirectedVertex* v3 = new DirectedVertex(4);
+
+    ig.addVertex(x);
+    ig.addVertex(v1);
+    ig.addVertex(v2);
+    ig.addVertex(v3);
+
+    ig.addEdge(x, v1);
+    ig.addEdge(x, v2);
+    ig.addEdge(x, v3);
+    ig.addEdge(v1, v2);
+
+    
+    DirectedDegreeDistribution<IndexedGraph, DirectedVertex> distribution(ig);
+    DirectedDegreeDistribution<IndexedGraph, DirectedVertex>::DistributionIterator itIn = distribution.inDegreeIterator();
+    DirectedDegreeDistribution<IndexedGraph, DirectedVertex>::DistributionIterator itOut = distribution.outDegreeIterator();
+
+
+    //there it should be 1 vertex of In degree 0
+    ASSERT_EQ(itIn->first, 0);
+    ASSERT_EQ(itIn->second, 1);
+
+    ++itIn;
+
+     //there it should be 2 vertex of In degree 1
+    ASSERT_EQ(itIn->first, 1);
+    ASSERT_EQ(itIn->second, 2);
+
+    ++itIn;
+
+     //there it should be 2 vertex of In degree 2
+    ASSERT_EQ(itIn->first, 2);
+    ASSERT_EQ(itIn->second, 1);
+
+    
+
+   
+    //there it should be 2 vertex of Out degree 0
+    ASSERT_EQ(itOut->first, 0);
+    ASSERT_EQ(itOut->second, 2);
+
+    ++itOut;
+
+     //there it should be 1 vertex of Out degree 1
+    ASSERT_EQ(itOut->first, 1);
+    ASSERT_EQ(itOut->second, 1);
+
+    ++itOut;
+
+     //there it should be 1 vertex of Out degree 3
+    ASSERT_EQ(itOut->first, 3);
+    ASSERT_EQ(itOut->second, 1);
+
+}
+
+
+TEST_F(DirectedDegreeDistributionTest, DirectedDegreeDistributionList)
+{
+
+   
+    ListGraph ig(true,false);
+    //Create vertex
+    DirectedVertex* x = new DirectedVertex(1);
+
+    //create neighbor vertices
+    DirectedVertex* v1 = new DirectedVertex(2);
+    DirectedVertex* v2 = new DirectedVertex(3);
+    DirectedVertex* v3 = new DirectedVertex(4);
+
+    ig.addVertex(x);
+    ig.addVertex(v1);
+    ig.addVertex(v2);
+    ig.addVertex(v3);
+
+    ig.addEdge(x, v1);
+    ig.addEdge(x, v2);
+    ig.addEdge(x, v3);
+    ig.addEdge(v1, v2);
+
+    
+    DirectedDegreeDistribution<ListGraph, DirectedVertex> distribution(ig);
+    DirectedDegreeDistribution<ListGraph, DirectedVertex>::DistributionIterator itIn = distribution.inDegreeIterator();
+    DirectedDegreeDistribution<ListGraph, DirectedVertex>::DistributionIterator itOut = distribution.outDegreeIterator();
+
+
+    //there it should be 1 vertex of In degree 0
+    ASSERT_EQ(itIn->first, 0);
+    ASSERT_EQ(itIn->second, 1);
+
+    ++itIn;
+
+     //there it should be 2 vertex of In degree 1
+    ASSERT_EQ(itIn->first, 1);
+    ASSERT_EQ(itIn->second, 2);
+
+    ++itIn;
+
+     //there it should be 2 vertex of In degree 2
+    ASSERT_EQ(itIn->first, 2);
+    ASSERT_EQ(itIn->second, 1);
+
+    
+
+   
+    //there it should be 2 vertex of Out degree 0
+    ASSERT_EQ(itOut->first, 0);
+    ASSERT_EQ(itOut->second, 2);
+
+    ++itOut;
+
+     //there it should be 1 vertex of Out degree 1
+    ASSERT_EQ(itOut->first, 1);
+    ASSERT_EQ(itOut->second, 1);
+
+    ++itOut;
+
+     //there it should be 1 vertex of Out degree 3
+    ASSERT_EQ(itOut->first, 3);
+    ASSERT_EQ(itOut->second, 1);
+
+}
+
+TEST_F(DirectedDegreeDistributionTest, DirectedDegreeDistributionVector)
+{
+    VectorGraph ig(true,false);
+    
+    //Create vertex
+    DirectedVertex* x = new DirectedVertex(1);
+
+    //create neighbor vertices
+    DirectedVertex* v1 = new DirectedVertex(2);
+    DirectedVertex* v2 = new DirectedVertex(3);
+    DirectedVertex* v3 = new DirectedVertex(4);
+
+    ig.addVertex(x);
+    ig.addVertex(v1);
+    ig.addVertex(v2);
+    ig.addVertex(v3);
+
+    ig.addEdge(x, v1);
+    ig.addEdge(x, v2);
+    ig.addEdge(x, v3);
+    ig.addEdge(v1, v2);
+
+    
+    DirectedDegreeDistribution<VectorGraph, DirectedVertex> distribution(ig);
+    DirectedDegreeDistribution<VectorGraph, DirectedVertex>::DistributionIterator itIn = distribution.inDegreeIterator();
+    DirectedDegreeDistribution<VectorGraph, DirectedVertex>::DistributionIterator itOut = distribution.outDegreeIterator();
+
+
+    //there it should be 1 vertex of In degree 0
+    ASSERT_EQ(itIn->first, 0);
+    ASSERT_EQ(itIn->second, 1);
+
+    ++itIn;
+
+     //there it should be 2 vertex of In degree 1
+    ASSERT_EQ(itIn->first, 1);
+    ASSERT_EQ(itIn->second, 2);
+
+    ++itIn;
+
+     //there it should be 2 vertex of In degree 2
+    ASSERT_EQ(itIn->first, 2);
+    ASSERT_EQ(itIn->second, 1);
+
+    
+
+   
+    //there it should be 2 vertex of Out degree 0
+    ASSERT_EQ(itOut->first, 0);
+    ASSERT_EQ(itOut->second, 2);
+
+    ++itOut;
+
+     //there it should be 1 vertex of Out degree 1
+    ASSERT_EQ(itOut->first, 1);
+    ASSERT_EQ(itOut->second, 1);
+
+    ++itOut;
+
+     //there it should be 1 vertex of Out degree 3
+    ASSERT_EQ(itOut->first, 3);
+    ASSERT_EQ(itOut->second, 1);
+
+}
+
+TEST_F(DirectedDegreeDistributionTest, DirectedDegreeDistributionRealWorldTest)
+{
+    VectorGraph * g = new VectorGraph(true, true);
+    GraphReader<VectorGraph, DirectedVertex> graphReader;
+
+    graphReader.read(*g, "TestTrees/DirectedTree.txt");
+
+    DirectedDegreeDistribution<VectorGraph, DirectedVertex> distribution(*g);
+    DirectedDegreeDistribution<VectorGraph, DirectedVertex>::DistributionIterator itIn = distribution.inDegreeIterator();
+    DirectedDegreeDistribution<VectorGraph, DirectedVertex>::DistributionIterator itOut = distribution.outDegreeIterator();
+
+
+    ASSERT_EQ(itOut->first,1);
+    ASSERT_EQ(itOut->second,2);
+    ++itOut;
+
+    ASSERT_EQ(itIn->first,2);
+    ASSERT_EQ(itIn->second,2);
+    ++itIn;
+
+    ASSERT_EQ(itOut->first,3);
+    ASSERT_EQ(itOut->second,3);
+    ++itOut;
+
+    ASSERT_EQ(itIn->first,3);
+    ASSERT_EQ(itIn->second,3);
+    ++itIn;
+
+    ASSERT_EQ(itOut->first,4);
+    ASSERT_EQ(itOut->second,2);
+    ++itOut;
+
+    ASSERT_EQ(itIn->first,4);
+    ASSERT_EQ(itIn->second,2);
+    ++itIn;
+
+    ASSERT_EQ(itOut->first,5);
+    ASSERT_EQ(itOut->second,1);
+    ++itOut;
+
+    ASSERT_EQ(itIn->first,5);
+    ASSERT_EQ(itIn->second,2);
+    ++itIn;
+
+    ASSERT_EQ(itOut->first,6);
+    ASSERT_EQ(itOut->second,1);
+    ++itOut;
+}
+
+}
--- trunk/test/readme.txt
+++ trunk/test/readme.txt
@@ -1,7 +1,25 @@
 In order to run the tests, execute
 
+Adding the previously mentioned requirements, install this other ones:
+
+``` sudo apt-get install cmake unzip ```
+
+And then to install the Gtest library
+
+``` 
+- wget http://googletest.googlecode.com/files/gtest-1.7.0.zip
+- unzip gtest-1.7.0.zip
+- cd gtest-1.7.0
+- ./configure
+- make
+- sudo cp -a include/gtest /usr/include
+- sudo cp -a lib/.libs/* /usr/lib/
+
+```
+Finnaly
+
+```
 cmake CMakeLists.txt
 make all
 ./tests
-
-
+```
