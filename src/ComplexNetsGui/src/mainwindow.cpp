645e7c539266352d4f86180d9fcd39052eb7bd06
Alexander Campos <alexjck@gmail.com>
2015-12-16 18:25:09 -0300

    Update mainwindow.cpp

--- src/ComplexNetsGui/src/mainwindow.cpp
+++ src/ComplexNetsGui/src/mainwindow.cpp
@@ -1,6 +1,6 @@
 // This toolbox is licensed under the Academic Free License 3.0.
 // Instituto Tecnológico de Buenos Aires (ITBA).
-// Last modification: December 19th, 2012.
+// Last modification: December 16th, 2015.
 
 #include <QFileDialog>
 #include <QMessageBox>
