5abe3df6b6fe4e3628dbdde41b326a91e2f5b34b
Federico Bond <federicobond@gmail.com>
2017-03-17 00:12:56 -0300

    Improve format.sh script

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,6 +3,6 @@
 command -v clang-format >/dev/null 2>&1 || {
   echo >&2 "clang-format is not installed.  Aborting."; exit 1; }
 
-exec find src -iname *.h -o -iname *.cpp |
+exec find src -iname "*.h" -o -iname "*.cpp" |
   grep -v "src/ComplexNets/mili/" |
   xargs clang-format -i
