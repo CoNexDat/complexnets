bda149c7d33326d9162e3af8653308fa8d98de7c
Federico Bond <federicobond@gmail.com>
2017-03-21 23:24:57 -0300

    Simplify GTest bootstrapping

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -1,14 +1,7 @@
-
 #include <gtest/gtest.h>
-//#include <gmock/gmock.h>
 
-namespace
-{
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::GTEST_FLAG(throw_on_failure) = true;
-    //::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
-}
 }
